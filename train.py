743435b6f29c507a47e1543cfd4daf32c244fb4e
BENMOUSSA SALMA <salma.benmoudssa@gmail.com>
2023-08-18 19:27:57 +0100

    Model

--- train.py
+++ train.py
@@ -1,37 +1,92 @@
 import json
 from nltk_utils import tokenize, stem, bag_of_words
 import numpy as np
-
-import torch
-
+import multiprocessing
+import torch  #une bibliothèque de calcul scientifique qui fournit des outils pour le calcul tensoriel (matrices multidimensionnelles) ainsi que des fonctionnalités pour la construction et l'entraînement de modèles d'apprentissage automatique
+import torch.nn as nn  #Les couches de neurones, les fonctions d'activation et autres composants de réseau sont définis dans ce module.
+from torch.utils.data import Dataset, DataLoader #Dataset: Cette classe abstraite sert de base pour créer des ensembles de données personnalisés. | DataLoader: Cette classe est utilisée pour créer un itérable sur un Dataset, ce qui permet de charger les données en batchs (groupes) pour l'entraînement.  
+from model import NeuralNet
 with open('intents.json','r') as f :
     intents = json.load(f)
 
-all_words = []
-tags = []
-xy = [] #patterns and tags
-for intent in intents['intents']: #the only array in intents.json intents is the key the first one is all the file
-    tag = intent['tag'] #tag is the key
-    tags.append(tag)
-    for pattern in intent['patterns']:
-        w = tokenize(pattern) 
-        all_words.extend(w) # I use extend because w it's also an array
-        xy.append((w, tag))
 
-ignore_words = ['?','!','.',',']
-all_words = [stem(w) for w in all_words if w not in ignore_words]
-all_words = sorted(set(all_words)) #set for remove duplicate elements 
-tags = sorted(set(tags)) #give a unique lables
-print(tags)
+def main():
+    all_words = []
+    tags = []
+    xy = [] #patterns and tags
+    for intent in intents['intents']: #the only array in intents.json intents is the key the first one is all the file
+        tag = intent['tag'] #tag is the key
+        tags.append(tag)
+        for pattern in intent['patterns']:
+            w = tokenize(pattern) 
+            all_words.extend(w) # I use extend because w it's also an array
+            xy.append((w, tag))
 
-x_train = []
-y_train = []
-for (pattern_sentece, tag) in xy:
-    bag = bag_of_words(pattern_sentece, all_words)
-    x_train.append(bag)
+    ignore_words = ['?','!','.',',']
+    all_words = [stem(w) for w in all_words if w not in ignore_words]
+    all_words = sorted(set(all_words)) #set for remove duplicate elements 
+    tags = sorted(set(tags)) #give a unique lables
+    print(tags)
 
-    label = tags.index(tag)
-    y_train.append(label) 
+    x_train = []
+    y_train = []
+    for (pattern_sentece, tag) in xy:
+        bag = bag_of_words(pattern_sentece, all_words)
+        x_train.append(bag)
 
-x_train = np.array(x_train)
-y_train = np.array(y_train)
+        label = tags.index(tag)
+        y_train.append(label) 
+
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+
+    class ChatDataset(Dataset):
+        def __init__(self):
+            self.n_samples = len(x_train)
+            self.x_data = x_train
+            self.y_data = y_train
+        #dataset[idx]
+        def __getitem__(self, index):
+            return self.x_data[index], self.y_data[index]
+        def __len__(self):
+            return self.n_samples
+
+    #Hyperparameters
+    batch_size = 8
+    hidden_size = 8
+    output_size = len(tags)
+    input_size = len(x_train[0])
+    learning_rate = 0.001
+    num_epochs = 1000
+
+    dataset = ChatDataset()
+    train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = NeuralNet(input_size, hidden_size, output_size).to(device) #push my model to device
+
+    #loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) # Create an Adam optimizer to update the model's parameters with the specified learning rate
+
+    for epoch in range(num_epochs):
+        for (words, labels) in train_loader:
+            words = words.to(device)
+            labels = labels.to(device)
+
+            #forward
+            outputs = model(words)
+            loss = criterion(outputs, labels)
+
+            #backward and optimizer step
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        if (epoch +1) % 100 == 0:
+            print(f'epoch {epoch+1}/{num_epochs}, loss={loss.item():.4f}')
+
+    print(f'final loss , loss={loss.item():.4f}')     
+
+if __name__ == '__main__':
+    multiprocessing.set_start_method('spawn')
+    main()
