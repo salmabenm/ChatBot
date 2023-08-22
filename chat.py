1172d2bb5cfa6c8cb02a9b5a7454d4f6e5a1d521
BENMOUSSA SALMA <salma.benmoudssa@gmail.com>
2023-08-22 13:53:34 +0100

    App

--- chat.py
+++ chat.py
@@ -8,10 +8,10 @@ from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('intents.json', 'r') as json_data: #training file
+with open('intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
-FILE = "data.pth" #save file
+FILE = "data.pth"
 data = torch.load(FILE)
 
 input_size = data["input_size"]
@@ -22,32 +22,27 @@ tags = data['tags']
 model_state = data["model_state"]
 
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
-model.load_state_dict(model_state) # loads the state dictionary model_state into my model. The state dictionary contains the learned parameters (weights and biases) of my model.
-model.eval() #sets my model to evaluation mode. In evaluation mode, the model behaves differently from training mode.
+model.load_state_dict(model_state)
+model.eval()
 
-bot_name = "Salma" #my bot name
-print("Let's chat! (type 'quit' to exit)")
-while True:
-    # sentence = "do you use credit cards?"
-    sentence = input("You: ") #the text enter by an user
-    if sentence == "quit":
-        break
+bot_name = "Sam"
 
-    sentence = tokenize(sentence)
+def get_response(msg):
+    sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
-    X = X.reshape(1, X.shape[0]) #shape[0] nbr of colums
+    X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
 
     output = model(X)
     _, predicted = torch.max(output, dim=1)
 
-    tag = tags[predicted.item()] #class label
+    tag = tags[predicted.item()]
 
-    probs = torch.softmax(output, dim=1) #for the prob of each outputs
+    probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
     if prob.item() > 0.75:
-        for intent in intents['intents']: #loop sur le file intents
+        for intent in intents['intents']:
             if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
-    else:
-        print(f"{bot_name}: I do not understand...")
+                return random.choice(intent['responses'])
+    
+    return "I do not understand..."
